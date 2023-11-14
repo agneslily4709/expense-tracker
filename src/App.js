2a4321e2b5a1a8ad81edb3f529b4607254181f0c
agneslily4709 <142089703+agneslily4709@users.noreply.github.com>
2023-11-14 21:55:04 +0530

    Revert "Add ExpenseTable Component"

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Input from './components/Input';
 import MonthSlider from './components/MonthSlider';
 import NavBar from './components/NavBar';
 import BudgetContext from './context/budgetContext';
-import ExpenseTable from './pages/ExpenseTable'
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
         <div className='home'>
                 <MonthSlider/>
                 <Input/>
-                <ExpenseTable/>
         </div>
         </BudgetContext>
     </div>
