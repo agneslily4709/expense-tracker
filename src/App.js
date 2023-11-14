8f241abf58b3ab2616dc1b9d6fe7dcff9c802aad
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-11-14 21:08:56 +0530

    Add BudgetContext Logic
    Introduce BudgetContext component for managing budget-related data. The component includes a context provider with sample data for user featuring income and expense entries. The associated columns provide structured display options. This addition lays the groundwork for efficient state management in budget-related functionalities across the application.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import './CustomSytles.css';
 import Input from './components/Input';
 import MonthSlider from './components/MonthSlider';
 import NavBar from './components/NavBar';
+import BudgetContext from './context/budgetContext';
 
 function App() {
   return (
     <div className="App">
+        <BudgetContext>
         <NavBar/>
         <div className='home'>
                 <MonthSlider/>
                 <Input/>
         </div>
+        </BudgetContext>
     </div>
   );
 }
