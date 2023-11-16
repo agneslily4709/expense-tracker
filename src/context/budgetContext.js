0519185b277359997654f3ac7b7614864834d8e2
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-11-16 23:53:17 +0530

    Implement ExpenseTable Render
    Worked on some fixes in the budgetContext logic and resolved few bugs. Ensured that ExpenseTable component renders properly.

--- src/context/budgetContext.js
+++ src/context/budgetContext.js
@@ -1,38 +1,42 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const BudgetDataContext = createContext(null)
 
+const expenseRows = [
+        { id: 101, date: "2 January", type: true, category: "Income", amount: 5000, description: "Salary Credited" },
+        { id: 102, date: "10 January", type: false, category: "Rent", amount: 200, description: "House rent and EB" },
+        { id: 103, date: "15 January", type: false, category: "Groceries", amount: 50, description: "Weekly grocery shopping" },
+        { id: 104, date: "20 January", type: true, category: "Bonus", amount: 1000, description: "Year-end bonus" },
+        { id: 105, date: "25 January", type: false, category: "Dining", amount: 75, description: "Dinner with friends" }
+      ]
+const expenseCols = [
+{ field: 'id', headerName: 'ID', width: 70 },
+{ field: 'firstName', headerName: 'First name', width: 130 },
+{ field: 'lastName', headerName: 'Last name', width: 130 },
+{
+field: 'age',
+headerName: 'Age',
+type: 'number',
+width: 90,
+},
+{
+field: 'fullName',
+headerName: 'Full name',
+description: 'This column has a value getter and is not sortable.',
+sortable: false,
+width: 160,
+}
+]
+
 const BudgetContext = ({children}) => {
-        let data = {
+        const [rows, setRows] = useState(expenseRows);
+        const data = {
                 userName:"Agnes Lily",
-                rows: [
-                        { id: 101, date: "2 January", type: "in", category: "Income", amount: 5000, description: "Salary Credited" },
-                        { id: 102, date: "10 January", type: "out", category: "Rent", amount: 200, description: "House rent and EB" },
-                        { id: 103, date: "15 January", type: "out", category: "Groceries", amount: 50, description: "Weekly grocery shopping" },
-                        { id: 104, date: "20 January", type: "in", category: "Bonus", amount: 1000, description: "Year-end bonus" },
-                        { id: 105, date: "25 January", type: "out", category: "Dining", amount: 75, description: "Dinner with friends" },
-                      ],
-                cols : [
-                        { field: 'id', headerName: 'ID', width: 70 },
-                        { field: 'firstName', headerName: 'First name', width: 130 },
-                        { field: 'lastName', headerName: 'Last name', width: 130 },
-                        {
-                          field: 'age',
-                          headerName: 'Age',
-                          type: 'number',
-                          width: 90,
-                        },
-                        {
-                          field: 'fullName',
-                          headerName: 'Full name',
-                          description: 'This column has a value getter and is not sortable.',
-                          sortable: false,
-                          width: 160,
-                          valueGetter: (params) =>
-                            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-                        },
-                      ]
+                rows: rows,
+                cols : expenseCols,
+                setRows:setRows
         }
+
         return (
                 <BudgetDataContext.Provider value={data}>{children}</BudgetDataContext.Provider>
         )
