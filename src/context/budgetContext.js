c6e5af3e28d1cded33834cbf644eb961ae19c788
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-11-21 23:26:44 +0530

    Update budgetContext.js

--- src/context/budgetContext.js
+++ src/context/budgetContext.js
@@ -3,11 +3,11 @@ import React, { createContext, useState } from "react";
 export const BudgetDataContext = createContext(null)
 
 const expenseRows = [
-        { id: 101, date: "2 January", type: true, category: "Income", amount: 5000, description: "Salary Credited" },
-        { id: 102, date: "10 January", type: false, category: "Rent", amount: 200, description: "House rent and EB" },
-        { id: 103, date: "15 January", type: false, category: "Groceries", amount: 50, description: "Weekly grocery shopping" },
-        { id: 104, date: "20 January", type: true, category: "Bonus", amount: 1000, description: "Year-end bonus" },
-        { id: 105, date: "25 January", type: false, category: "Dining", amount: 75, description: "Dinner with friends" }
+        { id: 101, date: "2-01-2023", type: true, category: "Income", amount: 5000, description: "Salary Credited" },
+        { id: 102, date: "10-01-2023", type: false, category: "Rent", amount: 200, description: "House rent and EB" },
+        { id: 103, date: "15-01-2023", type: false, category: "Groceries", amount: 50, description: "Weekly grocery shopping" },
+        { id: 104, date: "20-01-2023", type: true, category: "Bonus", amount: 1000, description: "Year-end bonus" },
+        { id: 105, date: "25-01-2023", type: false, category: "Dining", amount: 75, description: "Dinner with friends" }
       ]
 const expenseCols = [
 { field: 'id', headerName: 'ID', width: 70 },
@@ -30,11 +30,14 @@ width: 160,
 
 const BudgetContext = ({children}) => {
         const [rows, setRows] = useState(expenseRows);
+        const [editData, setEditData] = useState(null);
         const data = {
                 userName:"Agnes Lily",
                 rows: rows,
                 cols : expenseCols,
-                setRows:setRows
+                setRows:setRows,
+                editData:editData,
+                setEditData:setEditData
         }
 
         return (
