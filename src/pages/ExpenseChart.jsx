8f8fdaaa377136c0fde091be375da70d11e34df7
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-12-09 22:47:52 +0530

    Update ExpenseChart.jsx

--- src/pages/ExpenseChart.jsx
+++ src/pages/ExpenseChart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useContext } from 'react';
+import React, { useContext } from 'react';
 import { PieChart } from '@mui/x-charts/PieChart';
 import {BudgetDataContext} from "../context/budgetContext"
 import {Button} from "@mui/material"
