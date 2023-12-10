a9229cb9f908c23cf73c3083cd2925619f8c0818
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-12-10 22:35:45 +0530

    Update budgetContext.js

--- src/context/budgetContext.js
+++ src/context/budgetContext.js
@@ -2,13 +2,23 @@ import React, { createContext, useState } from "react";
 
 export const BudgetDataContext = createContext(null)
 
-const expenseRows = [
-        { id: 101, date: "2-01-2023", type: true, category: "Income", amount: 5000, description: "Salary Credited" },
-        { id: 102, date: "10-01-2023", type: false, category: "Rent", amount: 200, description: "House rent and EB" },
-        { id: 103, date: "15-01-2023", type: false, category: "Groceries", amount: 50, description: "Weekly grocery shopping" },
-        { id: 104, date: "20-01-2023", type: true, category: "Bonus", amount: 1000, description: "Year-end bonus" },
-        { id: 105, date: "25-01-2023", type: false, category: "Dining", amount: 75, description: "Dinner with friends" }
-      ]
+const expenseRows = {
+        "January":[
+                { id: 101, date: "2-01-2023", type: true, category: "Income", amount: 5000, description: "Salary Credited" },
+                { id: 102, date: "10-01-2023", type: false, category: "Rent", amount: 2000, description: "House rent and EB" },
+                { id: 103, date: "15-01-2023", type: false, category: "Utilities", amount: 150, description: "Weekly grocery shopping" },
+                { id: 104, date: "20-01-2023", type: true, category: "Bonus", amount: 1000, description: "Year-end bonus" },
+                { id: 105, date: "25-01-2023", type: false, category: "Food", amount: 75, description: "Dinner with friends" }
+        ],
+        "February":[
+                //Generate similarly with the category
+               // const categoryOptions = ["Income","Travel","Food","Entertainment","Bonus","Utilities","Mortage","Others"]
+                { id: 201, date: "5-02-2023", type: false, category: "Rent", amount: 2000, description: "House rent and EB" },
+                { id: 202, date: "12-02-2023", type: false, category: "Food", amount: 100, description: "Groceries for the month" },
+                { id: 203, date: "18-02-2023", type: false, category: "Entertainment", amount: 50, description: "Movie night" },
+                { id: 204, date: "25-02-2023", type: true, category: "Income", amount: 5500, description: "Salary Credited" },
+        ]
+}
 const expenseCols = [
 { field: 'id', headerName: 'ID', width: 70 },
 { field: 'firstName', headerName: 'First name', width: 130 },
