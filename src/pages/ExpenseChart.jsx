368e0f7b20efef9a0bc2a3a67fcde66c7a3faaa9
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-12-12 23:07:15 +0530

    Update ExpenseChart.jsx

--- src/pages/ExpenseChart.jsx
+++ src/pages/ExpenseChart.jsx
@@ -1,31 +1,36 @@
 import React, { useContext } from 'react';
 import { PieChart } from '@mui/x-charts/PieChart';
-import {BudgetDataContext} from "../context/budgetContext"
-import {Box, Button} from "@mui/material"
-const ExpenseChart = ({ setVisualize }) => {
-        const {rows,current} = useContext(BudgetDataContext)
-        const categoryData = rows[current.month].reduce((acc, expense) => {
-                if (expense.type === false) {
-                  const category = expense.category;
-                  acc[category] = (acc[category] || 0) + expense.amount;
-                }
-                return acc;
-              }, {});
+import { BudgetDataContext } from '../context/budgetContext';
+import { Box, Button } from '@mui/material';
 
-              const pieChartData = Object.entries(categoryData).map(([label, value], index) => ({
-                id: index,
-                value,
-                label,
-              }));
+const ExpenseChart = ({ setVisualize }) => {
+  const { rows, current } = useContext(BudgetDataContext);
+  console.log(rows[current.month]);
+  const categoryOptions = ['Rent','Income', 'Travel', 'Food', 'Entertainment', 'Bonus', 'Utilities', 'Mortgage', 'Others'];
+
+  const categoryData = categoryOptions.reduce((acc, category) => {
+    acc[category] = 0;
+    return acc;
+  }, {});
+  rows[current.month].forEach((expense) => {
+    if (expense.type === false) {
+      const category = expense.category;
+      categoryData[category] += expense.amount;
+    }
+  });
+console.log(categoryData);
+  const pieChartData = Object.entries(categoryData).map(([label, value], index) => ({
+    id: index,
+    value,
+    label,
+  }));
+
   return (
     <div>
-        <Button variant='contained' onClick={()=>setVisualize(false)}> Back </Button>
- <PieChart
-      series={[{ data: pieChartData }]}
-      width={700}
-      height={400}
-    />
-    <Box></Box>
+      <Button variant="contained" onClick={() => setVisualize(false)}>
+        Back
+      </Button>
+      <PieChart series={[{ data: pieChartData }]} width={700} height={400} />
     </div>
   );
 };
