1ec07a3fa90b1ab6c34fadeb7053b883457a1c50
AGNES LILY <142089703+agneslily4709@users.noreply.github.com>
2023-12-14 10:57:05 +0530

    Update ExpenseChart.jsx

--- src/pages/ExpenseChart.jsx
+++ src/pages/ExpenseChart.jsx
@@ -1,38 +1,51 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { PieChart } from '@mui/x-charts/PieChart';
 import { BudgetDataContext } from '../context/budgetContext';
-import { Box, Button } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 
 const ExpenseChart = ({ setVisualize }) => {
   const { rows, current } = useContext(BudgetDataContext);
 
   const categoryOptions = ['Rent', 'Travel', 'Food', 'Entertainment', 'Bonus', 'Utilities', 'Mortgage', 'Others'];
-const colorOptions = ["red","blue","green","orange","pink","yellow","purple","gray"]
-  const categoryData = categoryOptions.reduce((acc, category) => {
-    acc[category] = 0;
-    return acc;
-  }, {});
+  const colorOptions = ["red","blue","green","orange","pink","yellow","purple","gray"]
 
-  rows[current.month].forEach((expense) => {
-    if (expense.type === false) {
-      const category = expense.category;
-      categoryData[category] += expense.amount;
+  const [pieChartData, setPieChartData] = useState([]);
+
+  useEffect(() => {
+    if (rows[current.month]) {
+      const categoryData = categoryOptions.reduce((acc, category) => {
+        acc[category] = 0;
+        return acc;
+      }, {});
+
+      rows[current.month].forEach((expense) => {
+        if (expense.type === false) {
+          const category = expense.category;
+          categoryData[category] += expense.amount;
+        }
+      });
+
+      const newPieChartData = Object.entries(categoryData).map(([label, value], index) => ({
+        id: index,
+        value,
+        label,
+        color: colorOptions[index]
+      }));
+
+      setPieChartData(newPieChartData);
     }
-  });
+  }, [rows, current.month]);
 
-  const pieChartData = Object.entries(categoryData).map(([label, value], index) => ({
-    id: index,
-    value,
-    label,
-    color:colorOptions[index]
-  }));
-console.log(pieChartData);
   return (
     <div>
       <Button variant="contained" onClick={() => setVisualize(false)}>
         Back
       </Button>
-      <PieChart colors={colorOptions} series={[{ data: pieChartData }]} width={700} height={400} />
+      {rows[current.month] ? (
+        <PieChart colors={colorOptions} series={[{ data: pieChartData }]} width={700} height={400} />
+      ) : (
+        <Typography>No data</Typography>
+      )}
     </div>
   );
 };
